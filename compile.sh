ba2e6d8ef21a603ff73d6c480a3a729c86e32105
Kevin Carbaugh <kcarbaug@mines.edu>
2016-05-03 17:16:10 -0600

    Added platform aware compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 # -03
-nvcc -g -G -D NUM_SMS=15 -o gpuscale gpuscale.cu -arch=sm_35
+if [ `hostname` == "eecs-hpc-1" ]
+then
+  echo "Compiling for eecs-hpc-1 (Teslsa)"
+  nvcc -g -G -D NUM_SMS=15 -o gpuscale gpuscale.cu -arch=sm_35
+else
+  echo "Compiling for Issac (GTX 660)"
+  nvcc -g -G -D NUM_SMS=5 -o gpuscale gpuscale.cu -arch=sm_30
+fi
